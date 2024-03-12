ac45ed4b661b5c4e55195426d19a1f81cf2db351
anakhlf <137285214+anakhlf@users.noreply.github.com>
2024-03-12 19:59:40 +0100

    modification linkedin

--- src/common/containers/Footer/index.js
+++ src/common/containers/Footer/index.js
@@ -7,7 +7,7 @@ function Footer(){
                 <a href="mailto:khelif.anaispro@email.com" aria-label="Envoyer un e-mail à Anaïs Khelif">
                     <i className="fa-solid fa-envelope" ></i>
                 </a>
-                <a href="https://www.linkedin.com/anaïs-khelif/" aria-label="Profil LinkedIn d'Anaïs Khelif" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/ana%C3%AFs-khelif/" aria-label="Profil LinkedIn d'Anaïs Khelif" target="_blank" rel="noopener noreferrer">
                     <i className="fa-brands fa-linkedin-in"></i>
                 </a>
             </div>
